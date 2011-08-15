9d9115f2cb0e84ffae0d932e99b2cec2a811722d
Stefan Westerfeld <stefan@space.twc.de>
2011-08-16 01:16:43 +0200

    add manpage script

---
+++ rebuild-manpage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for p in bfsync.1
+do \
+  curl -sfS http://testbit.eu/$p?action=render >$p.web; \
+  curl -sfS "http://testbit.eu/index.php?title=$$p&action=raw" > $p.wiki; \
+  wikihtml2man.py $p.web >$p; \
+  groff -mandoc -Thtml < $p >$p.html; \
+  rm $p.web; \
+done
