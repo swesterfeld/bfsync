c9f8c8ea3bb2393b4b88fa8158125fb46db867ce
Stefan Westerfeld <stefan@space.twc.de>
2011-08-23 11:24:21 +0200

    fix getting wiki page

--- rebuild-manpage.sh
+++ rebuild-manpage.sh
@@ -2,7 +2,7 @@
 for p in bfsync.1
 do \
   curl -sfS http://testbit.eu/$p?action=render >$p.web; \
-  curl -sfS "http://testbit.eu/index.php?title=$$p&action=raw" > $p.wiki; \
+  curl -sfS "http://testbit.eu/index.php?title=$p&action=raw" > $p.wiki; \
   wikihtml2man.py $p.web >$p; \
   groff -mandoc -Thtml < $p >$p.html; \
   rm $p.web; \
