2b76735c64c74c7d071c985491ffc9312b62bf88
Stefan Westerfeld <stefan@space.twc.de>
2011-09-14 21:16:43 +0200

    improved file system tests

--- fs/fstest.py
+++ fs/fstest.py
@@ -15,21 +15,62 @@ def setup():
   if subprocess.call (["mkdir", "-p", "test/new"]) != 0:
     print "error during setup"
     sys.exit (1)
-  if subprocess.call (["mkdir", "-p", "test/data"]) != 0:
+  if subprocess.call (["mkdir", "-p", "test/data/subdir"]) != 0:
     print "error during setup"
     sys.exit (1)
-  if subprocess.call (["cp", "-aiv", "../README", "test/data/README"]) != 0:
+  if subprocess.call (["cp", "-a", "../README", "test/data/README"]) != 0:
     print "error during setup"
     sys.exit (1)
+  write_file ("test/data/subdir/x", "File X")
 
+def write_file (name, data):
+  f = open (name, "w")
+  f.write (data)
+  f.close()
+
+def read_file (name):
+  f = open (name)
+  data = f.read()
+  f.close()
+  return data
+
+# write/read test
 def test_01():
   bla = "blablabla\n*\nxyz\n"
   write_file ("testx", bla);
   if read_file ("testx") != bla:
-    print "test_01 failed"
-    sys.exit (1)
+    raise Exception ("read back failed")
+
+# Overwrite README
+def test_02():
+  bla = "blablabla\n*\nxyz\n"
+  write_file ("mnt/README", bla)
+  if read_file ("mnt/README") != bla:
+    raise Exception ("read back failed")
+  os.remove ("mnt/README")
+  if os.path.exists ("mnt/README"):
+    raise Exception ("File not properly deleted")
+
+# Write file in subdir
+def test_03():
+  bla = "blablabla\n*\nxyz\n"
+  write_file ("mnt/subdir/y", bla)
+  if read_file ("mnt/subdir/y") != bla:
+    raise Exception ("read back failed")
+
+tests = [ (test_01, "test_01"),
+          (test_02, "test_02"),
+          (test_03, "test_03") ]
+
+for (f, desc) in tests:
+  print desc, "...",
+  teardown()
+  setup()
+  try:
+    f()
+  except Exception, e:
+    print "FAIL: ", e
+  else:
+    print "OK."
 
 teardown()
-setup()
-test_01()
-
