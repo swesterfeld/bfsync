575a1f9a51fd5cb26848a82ce3fd4f9f73f874b3
Stefan Westerfeld <stefan@space.twc.de>
2011-09-18 20:30:51 +0200

    bfsync2: added StatusLine code

---
+++ fs/StatusLine.py
@@ -0,0 +1,21 @@
+import sys
+
+class StatusLine:
+  def __init__ (self):
+    self.op_text = ""
+    self.line = ""
+  def set_op (self, op):
+    self.op_text = op + ": "
+  def update (self, text):
+    print "\r",
+    # clear old status
+    for i in range (len (self.line)):
+      sys.stdout.write (" ")
+    self.line = self.op_text + text
+    print "\r%s " % self.line,
+    sys.stdout.flush()
+  def die (self, text):
+    if len (self.line) > 0:
+      print "\n"
+    sys.stderr.write ("bfsync: %s\n" % text)
+    sys.exit (1)
