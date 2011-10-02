4c81e5d52104db6c22dab6c8ea210d694fdda91f
Stefan Westerfeld <stefan@space.twc.de>
2011-10-02 21:18:34 +0200

    fs: added helper since python cannot call truncate (2) directly

---
+++ fs/bftesthelper.cc
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+int
+main (int argc, char **argv)
+{
+  if (argc == 4 && strcmp (argv[1], "truncate") == 0)
+    {
+      if (truncate (argv[2], atoi (argv[3])) == 0)
+        return 0;
+      else
+        {
+          printf ("truncate error: %s\n", strerror (errno));
+        }
+    }
+  return 1;
+}
