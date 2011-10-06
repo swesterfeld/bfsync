b2f0984b66e31713bc65f68f251d8e7ed5153ff3
Stefan Westerfeld <stefan@space.twc.de>
2011-10-06 22:44:32 +0200

    +setupdb

---
+++ fs/setupdb.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import sqlite3
+import os
+import time
+
+try:
+  os.remove ("test/db")
+except:
+  pass
+
+conn = sqlite3.connect ('test/db')
+c = conn.cursor()
+c.execute ('''create table inodes
+               (
+                 vmin     integer,
+                 vmax     integer,
+                 id       text,
+                 uid      integer,
+                 gid      integer,
+                 mode     integer,
+                 type     text,
+                 hash     text,
+                 ctime    integer,
+                 ctime_ns integer,
+                 mtime    integer,
+                 mtime_ns integer
+               )''')
+c.execute ('''create table links
+               (
+                 vmin     integer,
+                 vmax     integer,
+                 dir_id   text,
+                 inode_id text,
+                 name     text
+               )''')
+c.execute ('''create table history
+               (
+                 version integer,
+                 author  text,
+                 message text,
+                 time    integer
+               )''')
+c.execute ('''insert into history values (0, "", "", 0)''')
+
+time_now = int (time.time())
+c.execute ("""insert into inodes values (0, 0, "root", %d, %d, %d, "dir", "", %d, 0, %d, 0)""" % (os.getuid(), os.getgid(), 0755, time_now, time_now))
+conn.commit()
+c.close()
