cb193a61ff5bbb87b69b52fd62a5cb2742617495
Stefan Westerfeld <stefan@space.twc.de>
2011-10-20 20:08:02 +0200

    fs: get rid of extra subshell in multi run test

--- fs/multi-run-test.sh
+++ fs/multi-run-test.sh
@@ -2,12 +2,11 @@
 for run in $(seq 1 500)
 do
   echo "*** RUN $run ***"
-  (
-    export BFSYNC_NO_HASH_CACHE=1
-    mkdir run$run
-    cd run$run
-    /usr/bin/time -f "$run mkfiles-time %e" mkfiles.sh 
-    /usr/bin/time -f "$run commit-time  %e" bfsync2 commit
-  ) 2>&1
+  export BFSYNC_NO_HASH_CACHE=1
+  mkdir run$run
+  cd run$run
+  /usr/bin/time -f "$run mkfiles-time %e" mkfiles.sh 2>&1
+  /usr/bin/time -f "$run commit-time  %e" bfsync2 commit 2>&1
+  cd ..
 done
 
