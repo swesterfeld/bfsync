cdabcd1595bd1d4fb11b77fe1b0ba5773cde4907
Stefan Westerfeld <stefan@space.twc.de>
2011-10-26 15:56:23 +0200

    fs: switch diff output format to binary - fields/records separator is \0 now

--- fs/bfdiff.py
+++ fs/bfdiff.py
@@ -54,33 +54,51 @@ def key_to_str (k):
       s += " %s" % x
   return s
 
+def mklist (k):
+  l = []
+  for x in k:
+    l += [ "%s" % x ]
+  return l
+
+def write1change (change_list):
+  for s in change_list:
+    sys.stdout.write (s + "\0")
+
 def print_changes (change_type, dict_a, dict_b):
+  change_list = []
   for k in dict_a:
     if k in dict_b:
       if dict_a[k] == dict_b[k]:
-        pass    # identical
+        # identical
+        pass
       else:
         # attribute change
-        print change_type, "!", key_to_str (k),
+        change = [ change_type + "!"] + mklist (k)
         for i in range (len (dict_b[k])):
           if dict_a[k][i] == dict_b[k][i]:
-            print "-",
+            change += [ "" ]
           else:
-            print dict_b[k][i],
-        print
+            change += [ "%s" % dict_b[k][i] ]
+        change_list += [ change ]
     else:
-      print change_type, "-", key_to_str (k)                 # entry deleted
+      # entry deleted
+      change_list += [ [ change_type + "-" ] + mklist (k) ]
 
   for k in dict_b:
     if k in dict_a:
-      pass                          # attribute change <-> handled above
+      # attribute change <-> handled above
+      pass
     else:
-      print change_type, "+", key_to_str (k), key_to_str (dict_b[k])      # entry added
+      # entry added
+      change_list += [ [ change_type + "+" ] + mklist (k) + mklist (dict_b[k]) ]
+  # sort changes (for better compression)
+  change_list.sort()
+  # write changes to stdout
+  for change in change_list:
+    write1change (change)
 
 dict_a = dict_from_table ("links", 2, version_a)
-print "======================================================================="
 dict_b = dict_from_table ("links", 2, version_b)
-print "======================================================================="
 print_changes ("l", dict_a, dict_b)
 
 dict_a = dict_from_table ("inodes", 1, version_a)
