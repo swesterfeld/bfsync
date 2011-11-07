d15555923d8744987aa8685b578783d7916e286c
Stefan Westerfeld <stefan@space.twc.de>
2011-11-07 14:44:26 +0100

    fs: use VACUUM to shrink db in setupdb.py

--- fs/setupdb.py
+++ fs/setupdb.py
@@ -87,6 +87,7 @@ else:
   c.execute ('''DELETE FROM inodes''')
   c.execute ('''DELETE FROM links''')
   c.execute ('''DELETE FROM history''')
+  c.execute ('''VACUUM''')
 
 c.execute ('''PRAGMA default_cache_size=%d''' % (1024 * 1024))     # use 128M cache size
 c.execute ('''INSERT INTO history VALUES (1, "", "", "", 0)''')
