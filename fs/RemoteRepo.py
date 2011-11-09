69a9aa069160fee6124c6d92af10845d961a8f30
Stefan Westerfeld <stefan@space.twc.de>
2011-11-09 16:48:41 +0100

    fs: make local pull/push/clone work

--- fs/RemoteRepo.py
+++ fs/RemoteRepo.py
@@ -29,6 +29,26 @@ class RemoteRepo:
     remote_history = pickle.loads (remote_p)
     return remote_history
 
+  def update_history (self, delta_history):
+    if self.conn == LOCAL:
+      command = [ "bfsync2", "remote-history-update", self.path ]
+    else:
+      command = [ "ssh", self.host, "bfsync2", "remote-history-update", self.path]
+
+    remote_send_p = subprocess.Popen (command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    remote_send_p.stdin.write (pickle.dumps (delta_history))
+    return remote_send_p.communicate()[0]
+
+  def need_objects (self):
+    if self.conn == LOCAL:
+      command = [ "bfsync2", "remote-need-objects", self.path ]
+    else:
+      command = [ "ssh", self.host, "bfsync2", "remote-need-objects", self.path ]
+
+    need_objs_p = subprocess.Popen (command, stdout = subprocess.PIPE).communicate()[0]
+    need_objs = pickle.loads (need_objs_p)
+    return need_objs
+
   def ls (self):
     if self.conn == LOCAL:
       command = ["bfsync2", "remote-ls", self.path]
@@ -48,3 +68,12 @@ class RemoteRepo:
     remote_send_p = subprocess.Popen (command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     tlist.send_list (remote_send_p.stdin)
     tlist.receive_files (remote_send_p.stdout, True)
+
+  def put_objects (self, tlist):
+    if self.conn == LOCAL:
+      command = [ "bfsync2", "remote-receive" ]
+    else:
+      command = ["ssh", self.host, "bfsync2", "remote-receive" ]
+    pipe = subprocess.Popen (command, bufsize=-1, stdin=subprocess.PIPE).stdin
+    tlist.send_list (pipe)
+    tlist.send_files (pipe, True)
