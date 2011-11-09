3ebc94a85dc8d444e9eb21445e7bf360397df412
Stefan Westerfeld <stefan@space.twc.de>
2011-11-09 16:29:46 +0100

    fs: more work on pull/clone/... for local urls

--- fs/RemoteRepo.py
+++ fs/RemoteRepo.py
@@ -28,3 +28,23 @@ class RemoteRepo:
     remote_p = subprocess.Popen (command, stdout=subprocess.PIPE).communicate()[0]
     remote_history = pickle.loads (remote_p)
     return remote_history
+
+  def ls (self):
+    if self.conn == LOCAL:
+      command = ["bfsync2", "remote-ls", self.path]
+    else:
+      command = ["ssh", self.host, "bfsync2", "remote-ls", self.path]
+
+    remote_p = subprocess.Popen (command, stdout=subprocess.PIPE).communicate()[0]
+    remote_list = pickle.loads (remote_p)
+    return remote_list
+
+  def get_objects (self, tlist):
+    if self.conn == LOCAL:
+      command = [ "bfsync2", "remote-send" ]
+    else:
+      command = [ "ssh", self.host, "bfsync2", "remote-send" ]
+
+    remote_send_p = subprocess.Popen (command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    tlist.send_list (remote_send_p.stdin)
+    tlist.receive_files (remote_send_p.stdout, True)
