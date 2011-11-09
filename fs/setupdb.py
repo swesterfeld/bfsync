e92c0440cc6cb26b8fb70032b619ef1a7d0a64df
Stefan Westerfeld <stefan@space.twc.de>
2011-11-09 12:32:08 +0100

    fs: implemented bfsync2 clone

--- fs/setupdb.py
+++ fs/setupdb.py
@@ -4,6 +4,7 @@ import sqlite3
 import os
 import time
 import CfgParser
+from dbutils import create_tables
 
 def parse_config (filename):
   bfsync_info = CfgParser.CfgParser (filename,
@@ -37,51 +38,7 @@ c = conn.cursor()
 if not sqlite_sync:
   c.execute ('''PRAGMA synchronous=off''')
 if reinit_tables:
-  c.execute ('''CREATE TABLE inodes
-                 (
-                   vmin     integer,
-                   vmax     integer,
-                   id       text,
-                   uid      integer,
-                   gid      integer,
-                   mode     integer,
-                   type     text,
-                   hash     text,
-                   link     text,
-                   size     integer,
-                   major    integer,
-                   minor    integer,
-                   nlink    integer,    /* number of hard links */
-                   ctime    integer,
-                   ctime_ns integer,
-                   mtime    integer,
-                   mtime_ns integer
-                 )''')
-  c.execute ('''CREATE INDEX inodes_idx ON inodes (id)''')
-  c.execute ('''CREATE TABLE links
-                 (
-                   vmin     integer,
-                   vmax     integer,
-                   dir_id   text,
-                   inode_id text,
-                   name     text
-                 )''')
-  c.execute ('''CREATE INDEX links_idx ON links (dir_id)''')
-  c.execute ('''CREATE TABLE history
-                 (
-                   version integer,
-                   hash    text,
-                   author  text,
-                   message text,
-                   time    integer
-                 )''')
-  c.execute ('''CREATE TABLE local_inodes
-                 (
-                   id      text,
-                   ino     integer
-                 )''')
-  c.execute ('''CREATE INDEX local_inodes_idx_id ON local_inodes (id)''')
-  c.execute ('''CREATE INDEX local_inodes_idx_ino ON local_inodes (ino)''')
+  create_tables (c)
 else:
   c.execute ('''DELETE FROM local_inodes''')
   c.execute ('''DELETE FROM inodes''')
@@ -89,8 +46,7 @@ else:
   c.execute ('''DELETE FROM history''')
   c.execute ('''VACUUM''')
 
-c.execute ('''PRAGMA default_cache_size=%d''' % (1024 * 1024))     # use 128M cache size
-c.execute ('''INSERT INTO history VALUES (1, "", "", "", 0)''')
+init_tables (c)
 
 time_now = int (time.time())
 c.execute ("""INSERT INTO inodes VALUES (1, 1, "0000000000000000000000000000000000000000", %d, %d, %d, "dir", "", "", 0, 0, 0, 1, %d, 0, %d, 0)""" % (
