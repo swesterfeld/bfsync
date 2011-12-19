9df67f6ebfbd24c368e8551dc2b207c243819c67
Stefan Westerfeld <stefan@space.twc.de>
2011-12-19 16:49:11 +0100

    put bfsync implementation in installable package

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup (
   name = 'bfsync',
-  version = '0.1.1',
+  version = '0.2.0',
   description = 'Big File synchronization based on Git',
   author = 'Stefan Westerfeld',
   author_email = 'stefan@space.twc.de',
   url = 'http://space.twc.de/~stefan/bfsync.php',
-  scripts = ['bfsync'],
-  data_files=[('man/man1', ['bfsync.1'])]
+  package_dir = {
+    'bfsync': 'src/bfsync'
+  },
+  packages = ['bfsync'],
+  data_files=[('man/man1', ['bfsync.1'])],
+  entry_points = {
+    "console_scripts": [ "bfsync = bfsync.main:main" ]
+  }
 )
