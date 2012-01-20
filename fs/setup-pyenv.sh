7b8a7993ecc6c184a10d549adac53e9dd8e67d10
Stefan Westerfeld <stefan@space.twc.de>
2012-01-20 13:48:20 +0100

    pyenv: don't hardcode source tree location

--- fs/setup-pyenv.sh
+++ fs/setup-pyenv.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=$PWD/build/lib.linux-x86_64-2.7:$PYTHONPATH
 export LD_LIBRARY_PATH=$PWD/.libs:$LD_LIBRARY_PATH
-export PATH=$PATH:$HOME/src/bfsync/src
+export PATH=$PATH:$PWD/../src
