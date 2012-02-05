4c0498fc5ba5ce89d41b62dc0acba62d199571fb
Stefan Westerfeld <stefan@space.twc.de>
2012-02-05 11:40:18 +0100

    mkfiles.sh: improve prefix code

--- src/mkfiles.sh
+++ src/mkfiles.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+if [ "x$1" != "x" ]; then
+  p="$1:"
+else
+  p=""
+fi
+
 for i in $(seq 1 100)
 do
-  echo -ne "\rmkfiles: $1:$i/100"
+  echo -ne "\rmkfiles: $p$i/100"
   mkdir $i
   for j in $(seq 1 1000)
   do
-    echo "$1:$i/$j" > $i/$j
+    echo "$p$i/$j" > $i/$j
   done
 done
 echo -e "\r                                \rmkfiles: done."
