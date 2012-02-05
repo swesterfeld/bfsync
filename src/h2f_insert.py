b9b04536b588c23db19032063cfb9a9cda3c4479
Stefan Westerfeld <stefan@space.twc.de>
2012-02-05 12:08:34 +0100

    added performance test for hash->file database

---
+++ src/h2f_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import bfsyncdb
+import sys
+import hashlib
+import time
+
+bdb = bfsyncdb.open_db (sys.argv[1], 256, False)
+for j in range (1, 10000):
+  start = time.time()
+  bdb.begin_transaction()
+  for n in range (1, 20000):
+    hash = hashlib.sha1 ("%x:%x" % (j, n)).hexdigest()
+    bdb.store_hash2file (hash, n)
+  bdb.commit_transaction()
+  now = time.time()
+  print "%d %.2f" % (j * 20000, now - start)
+  sys.stdout.flush()
