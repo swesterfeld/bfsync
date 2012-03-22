1882e694e8a6c0951e9439efefb94fadbad3cca0
Stefan Westerfeld <stefan@space.twc.de>
2012-03-22 22:15:02 +0100

    BUILD: use pkg-config values for glib-2.0 package during python extension compile

--- fs/setup.py
+++ fs/setup.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python
 
 from setuptools import setup, Extension
+from subprocess import Popen, PIPE
+
+def pkg_config(*packages):
+  cmd = ['pkg-config', '--cflags', '--libs'] + list (packages)
+  proc = Popen (cmd, stdout=PIPE, stderr=PIPE)
+  output, error = proc.stdout.read(), proc.stderr.read()
+  if error:
+    raise ValueError (error)
+  config = { "-I" : [], "-L" : [], "-l" : []}
+  for token in output.split():
+    flag, value = token[:2], token[2:]
+    config[flag].append (value)
+  return config
+
+pkg_config_values = pkg_config ("glib-2.0")
 
 bfsyncdb_module = Extension('_bfsyncdb',
                            extra_compile_args = ['-D_FILE_OFFSET_BITS=64'],
                            sources=['bfsyncdb.cc', 'bfsyncdb.i'],
-                           library_dirs = ['.libs'],
-                           libraries=['bfsync'],
-                           include_dirs=['/usr/local/BerkeleyDB.5.3/include',
-                                         '/usr/include/glib-2.0', '/usr/lib/x86_64-linux-gnu/glib-2.0/include' ],
+                           library_dirs = ['.libs'] + pkg_config_values["-L"],
+                           libraries=['bfsync'] + pkg_config_values["-l"],
+                           include_dirs=['/usr/local/BerkeleyDB.5.3/include'] + pkg_config_values["-I"],
                            language='c++',
                            swig_opts=['-c++']
                            )
 
 setup (name = 'bfsyncdb',
-       version = '0.1',
-       author      = "SWIG Docs",
-       description = """Simple swig example from docs""",
+       version = '0.3.0',
+       author = 'Stefan Westerfeld',
+       author_email = 'stefan@space.twc.de',
+       description = 'Big File synchronization tool - python binding',
        ext_modules = [bfsyncdb_module],
        py_modules = ["bfsyncdb"],
        )
