427d2153768fc63b922f577c8b28badafab5f9f5
Stefan Westerfeld <stefan@space.twc.de>
2012-04-23 02:50:44 +0200

    bfsync xzutils: always use compression level -6 (not -9) to save memory during big commits

--- src/bfsync/xzutils.py
+++ src/bfsync/xzutils.py
@@ -15,37 +15,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# the manpage says that levels > "-6" only improve compression ratio if the file
-# is bigger than 8M
-
 import lzma
 import os
 
-compressor6 = None
-compressor9 = None
-
-def xz_compressor_for_file (filename):
-  global compressor6, compressor9
-
-  size = os.path.getsize (filename)
-  if (size <= 8 * 1024 * 1024):
-    opts = {'format':'xz', 'level':6}
-    if compressor6 is None:
-      compressor6 = lzma.LZMACompressor (opts)
-    else:
-      compressor6.reset (opts)
-    return compressor6
-  else:
-    opts = {'format':'xz', 'level':9}
-    if compressor9 is None:
-      compressor9 = lzma.LZMACompressor (opts)
-    else:
-      compressor9.reset (opts)
-
-    return compressor9
+compressor = None
 
 def xz (filename):
-  compressor = xz_compressor_for_file (filename)
+  global compressor
+
+  opts = {'format':'xz', 'level':6}
+  if compressor is None:
+    compressor = lzma.LZMACompressor (opts)
+  else:
+    compressor.reset (opts)
 
   f = open (filename, "r")
   xz_f = open ("%s.xz" % filename, "w")
