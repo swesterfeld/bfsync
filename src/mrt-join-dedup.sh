6c3a91f9361c386fe0339af9ec9f4f34d5bdfd96
Stefan Westerfeld <stefan@space.twc.de>
2012-06-01 16:22:32 +0200

    tests: fix multi-run-join-dedup test: don't write files in parent fs on error

--- src/mrt-join-dedup.sh
+++ src/mrt-join-dedup.sh
@@ -6,21 +6,24 @@ usage()
   exit 1
 }
 
+die()
+{
+  echo "$1"
+  exit 1
+}
+
 [ -d "$1" ] || usage;
 [ -d "$1/.bfsync" ] || usage;
 
 MKFILES=$PWD/mkfiles.sh
 BFSYNC=$PWD/bfsync.py
 
-cd $1
-
 for run in $(seq 1 500)
 do
   echo "*** RUN $run ***"
   export BFSYNC_NO_HASH_CACHE=1
-  mkdir run$run
-  cd run$run
-  /usr/bin/time -f "$run add-time %e" bash -c "$MKFILES run$run; $BFSYNC commit -m 'test run $run'" 2>&1
-  cd ..
+  mkdir $1/run$run || die "mkdir failed"
+  cd $1/run$run || die "chdir failed"
+  /usr/bin/time -f "$run add-time %e" bash -c "$MKFILES run$run; $BFSYNC commit -m 'test run $run'" 2>&1 || die "time failed"
 done
 
