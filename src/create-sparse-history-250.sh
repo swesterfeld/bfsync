65bd2a2afc8ea07066fcb5ffe6ce9fe7b86967c0
Stefan Westerfeld <stefan@space.twc.de>
2012-06-08 17:34:50 +0200

    test: added script to create 250-item sparse history

---
+++ src/create-sparse-history-250.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+usage()
+{
+  echo "create-sparse-history.sh <mount-point>"
+  exit 1
+}
+
+die()
+{
+  echo "$1"
+  exit 1
+}
+
+[ -d "$1" ] || usage;
+[ -d "$1/.bfsync" ] || usage;
+
+BFSYNC=$PWD/bfsync.py
+
+for run in $(seq 1 250)
+do
+  rm -f $1/run*
+  echo "run$run" > $1/run$run || die "file creation failed"
+  cd $1 || die "chdir failed"
+  $BFSYNC commit -m "test run $run" || die "commit failed"
+  $BFSYNC debug-change-time $((run + 1)) $(((run / 4) * 24 + run % 4)) || die "change time failed"
+done
+
+$BFSYNC push
