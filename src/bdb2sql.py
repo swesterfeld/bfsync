cdce80c8d2fd0af4b99d78059e060f7608c26faa
Stefan Westerfeld <stefan@space.twc.de>
2012-09-06 17:45:41 +0200

    bdb2sql.py: clean up output messages

--- src/bdb2sql.py
+++ src/bdb2sql.py
@@ -3,6 +3,7 @@
 import psycopg2 as dbapi2
 import bfsyncdb
 from bfsync.utils import *
+from bfsync.StatusLine import status_line, OutputSubsampler
 
 conn = dbapi2.connect (database="bfsync", user="postgres", host="bigraidn1", port=5410) #, password="python")
 cur = conn.cursor()
@@ -46,6 +47,10 @@ repo = cd_repo_connect_db()
 ai = bfsyncdb.AllINodesIterator (repo.bdb)
 
 ops = 0
+outss = OutputSubsampler()
+
+def update_status():
+  status_line.update ("%d" % ops)
 
 while True:
   id = ai.get_next()
@@ -57,8 +62,9 @@ while True:
   for link in links:
     cur.execute("INSERT INTO links (dir_id, vmin, vmax, inode_id, name) VALUES (%s, %s, %s, %s, %s)",  (
                 link.dir_id.str(), link.vmin, link.vmax, link.inode_id.str(), link.name))
-    print "\r%d" % ops,
     ops += 1
+    if outss.need_update():
+      update_status()
   for inode in inodes:
     cur.execute ("""INSERT INTO inodes (id, vmin, vmax, uid, gid, mode, type, hash, link, size, major, minor, nlink, ctime, ctime_ns,
                                         mtime, mtime_ns, new_file_number)
@@ -66,22 +72,27 @@ while True:
                  inode.id.str(), inode.vmin, inode.vmax, inode.uid, inode.gid, inode.mode, inode.type, inode.hash, inode.link,
                  inode.size, inode.major, inode.minor, inode.nlink, inode.ctime, inode.ctime_ns, inode.mtime, inode.mtime_ns,
                  inode.new_file_number))
-    print "\r%d" % ops,
     ops += 1
+    if outss.need_update():
+      update_status()
 
-cur.execute("SELECT * FROM links;")
+update_status()
+
+print
+
+cur.execute("SELECT COUNT (*) FROM links;")
 while True:
   row = cur.fetchone()
   if not row:
     break
-  print row
+  print "LINKS:", row
 
-cur.execute("SELECT * FROM inodes;")
+cur.execute("SELECT COUNT (*) FROM inodes;")
 while True:
   row = cur.fetchone()
   if not row:
     break
-  print row
+  print "INODES:", row
 
 
 conn.commit()
