85d21acd6c3c8f42d21102045d8569256de822e4
Stefan Westerfeld <stefan@space.twc.de>
2012-09-14 14:40:44 +0200

    bdb2sql2: fix locking problem, added better status output, increased transaction splitting boundary

--- src/bdb2sql2.py
+++ src/bdb2sql2.py
@@ -6,7 +6,7 @@ from bfsync.diffutils import DiffIterator
 import psycopg2 as dbapi2
 from bfsync.StatusLine import status_line, OutputSubsampler
 
-conn = dbapi2.connect (database="bfsync", user="postgres", host="bigraidn1", port=5410) #, password="python")
+conn = dbapi2.connect (database="bfsync", user="postgres") # , host="bigraidn1", port=5410) #, password="python")
 cur = conn.cursor ()
 cur.execute ("DROP TABLE IF EXISTS history")
 cur.execute ("""
@@ -55,13 +55,16 @@ while True:
 #print bdb_max_version, sql_max_version
 
 ops = 0
+del_scan_ops = 0
+scan_ops = 0
 outss = OutputSubsampler()
 start_time = time.time()
 
 def update_status():
   now_time = time.time()
-  status_line.update ("version %d/%d - imported changes: %d - rate %.2f changes/sec" % (
-    version, bdb_max_version, ops, ops / (now_time - start_time)
+  status_line.update ("version %d/%d - %d | %d - imported changes: %d - rate %.2f | %.2f changes/sec" % (
+    version, bdb_max_version, scan_ops, del_scan_ops, ops, ops / (now_time - start_time),
+                              scan_ops / (now_time - start_time)
   ))
 
 def same_data (d1, d2):
@@ -70,20 +73,26 @@ def same_data (d1, d2):
           d1.type == d2.type and d1.size == d2.size and d1.hash == d2.hash)
 
 ## clear old sql export entries
+n_clear = 0
 while True:
   repo.bdb.begin_transaction()
   deleted = repo.bdb.sql_export_clear (20000)
   repo.bdb.commit_transaction()
+  n_clear += deleted
+  status_line.update ("clear old sql export table... %d" % n_clear)
 
   if deleted == 0: # sql export table empty
     break
+
+status_line.update ("clear old sql export table... done.")
+status_line.cleanup()
 
 TRANS_OPS = 0
 def maybe_split_transaction():
   global TRANS_OPS
 
   TRANS_OPS += 1
-  if TRANS_OPS >= 10000:
+  if TRANS_OPS >= 20000:
     TRANS_OPS = 0
     repo.bdb.commit_transaction()
     repo.bdb.begin_transaction()
@@ -99,7 +108,7 @@ for version in range (sql_max_version + 1, bdb_max_version + 1):
       return data.parent_id.str()
 
   def walk (inode, filename, parent_id):
-    global ops
+    global ops, scan_ops
 
     if inode.valid:
       old_data = repo.bdb.sql_export_get (filename)
@@ -127,8 +136,6 @@ for version in range (sql_max_version + 1, bdb_max_version + 1):
           data.vmin = version
           data.vmax = bfsyncdb.VERSION_INF
           ops += 1
-          if outss.need_update():
-            update_status()
       else:
         fields = (filename, version, bfsyncdb.VERSION_INF, data.id.str(), get_parent (data), data.type, data.hash, data.size)
         cur.execute ("""INSERT INTO files (filename, vmin, vmax, id, parent_id, type, hash, size)
@@ -137,9 +144,10 @@ for version in range (sql_max_version + 1, bdb_max_version + 1):
         data.vmax = bfsyncdb.VERSION_INF
         status = "new"
         ops += 1
-        if outss.need_update():
-          update_status()
 
+      scan_ops += 1
+      if outss.need_update():
+        update_status()
       repo.bdb.sql_export_set (data)
       maybe_split_transaction()
 
@@ -154,9 +162,12 @@ for version in range (sql_max_version + 1, bdb_max_version + 1):
           walk (child_inode, inode_name, inode.id)
 
   def check_deleted():
-    global ops
+    global ops, del_scan_ops
 
+    repo.bdb.begin_transaction()
     delete_list_filename = repo.make_temp_name()
+    repo.bdb.commit_transaction()
+
     delete_list = open (delete_list_filename, "w")
 
     # prepare list of deleted files (and update sql db)
@@ -170,12 +181,11 @@ for version in range (sql_max_version + 1, bdb_max_version + 1):
       data = sxi.get_next()
       if not data.valid:
         break
+      del_scan_ops += 1
       if data.export_version != version:
         #print "%-60s%-12d%42s -- %s" % (data.filename, data.size, data.hash, "del")
         cur.execute ("UPDATE files SET vmax = %s WHERE filename = %s AND vmin = %s", (version - 1, data.filename, data.vmin))
         ops += 1
-        if outss.need_update():
-          update_status()
 
         # string escaping ensures that lines will never contain newline chars, even if the filename does
         delete_list.write (data.filename.encode ("string-escape") + "\n")
@@ -186,6 +196,7 @@ for version in range (sql_max_version + 1, bdb_max_version + 1):
 
     del sxi # close cursor
 
+    repo.bdb.begin_transaction()
     # remove deleted files from sql export table
     delete_list = open (delete_list_filename, "r")
     for filename in delete_list:
@@ -193,14 +204,15 @@ for version in range (sql_max_version + 1, bdb_max_version + 1):
       repo.bdb.sql_export_delete (filename)
       maybe_split_transaction()
     delete_list.close()
+    repo.bdb.commit_transaction()
 
   repo.bdb.begin_transaction()
   id = bfsyncdb.id_root()
   id_none = bfsyncdb.ID ("/" + 40 * "f")
   inode = repo.bdb.load_inode (id, version)
   walk (inode, "/", id_none)
-  check_deleted()
   repo.bdb.commit_transaction()
+  check_deleted()
 update_status()
 
 conn.commit()
