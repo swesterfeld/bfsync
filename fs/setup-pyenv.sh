13b70c7f51396aa0c5d32db9a44ab344a12eaa57
Stefan Westerfeld <stefan@space.twc.de>
2013-03-19 12:44:16 +0100

    FS: add path containing bfsyncfs binary to path

--- fs/setup-pyenv.sh
+++ fs/setup-pyenv.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=$PWD/`python setup-pyenv-helper.py`:$PYTHONPATH
 export LD_LIBRARY_PATH=$PWD/.libs:$LD_LIBRARY_PATH
-export PATH=$PATH:$PWD/../src
+export PATH=$PATH:$PWD/../src:$PWD
 export PS1="[py$(echo "$PWD $(hostname)" | sha1sum | sed 's/^\(..\).*/\1/g')] $PS1"
