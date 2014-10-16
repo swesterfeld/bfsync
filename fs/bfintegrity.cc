466a78c081e024504731689f4801ef46bfc4e325
Stefan Westerfeld <stefan@space.twc.de>
2014-10-16 12:25:23 +0200

    Integrity test: add link table pass.

--- fs/bfintegrity.cc
+++ fs/bfintegrity.cc
@@ -6,6 +6,7 @@ using std::string;
 using std::vector;
 
 using BFSync::BDB_TABLE_INODES;
+using BFSync::BDB_TABLE_LINKS;
 using BFSync::DataBuffer;
 using BFSync::DbcPtr;
 
@@ -19,6 +20,7 @@ class IntegrityCheck
   IDMap     id_map;
 
   void read_all_ids();
+  void check_links();
 
 public:
   IntegrityCheck (BDBPtr bdb_ptr);
@@ -95,11 +97,90 @@ IntegrityCheck::read_all_ids()
   printf ("\n");
 }
 
+static void
+links_update_status (size_t n_links)
+{
+  printf ("\rINTEGRITY: phase 2/2: checking links: %zd", n_links);
+  fflush (stdout);
+}
+
+void
+IntegrityCheck::check_links()
+{
+  DbcPtr dbc (bdb_ptr.get_bdb());   // Acquire a cursor for the database.
+
+  Dbt key;
+  Dbt data;
+  Dbt multi_data;
+
+  std::vector<char> multi_data_buffer (64 * 1024);
+
+  multi_data.set_flags (DB_DBT_USERMEM);
+  multi_data.set_data (&multi_data_buffer[0]);
+  multi_data.set_ulen (multi_data_buffer.size());
+
+  size_t n_links = 0;
+  links_update_status (0);
+
+  int dbc_ret = dbc->get (&key, &multi_data, DB_FIRST | DB_MULTIPLE_KEY);
+  while (dbc_ret == 0)
+    {
+      DbMultipleKeyDataIterator data_iterator (multi_data);
+
+      while (data_iterator.next (key, data))
+        {
+          char table = ((char *) key.get_data()) [key.get_size() - 1];
+          if (table == BDB_TABLE_LINKS)
+            {
+              DataBuffer kbuffer ((char *) key.get_data(), key.get_size());
+              BFSync::ID id (kbuffer);
+
+              DataBuffer dbuffer ((char *) data.get_data(), data.get_size());
+
+              unsigned int vmin = dbuffer.read_uint32();
+              unsigned int vmax = dbuffer.read_uint32();
+              BFSync::ID inode_id (dbuffer);
+              string name = dbuffer.read_string();
+
+              IDMap::iterator id_it;
+
+              id_it = id_map.find (id);
+              if (id_it == id_map.end())
+                {
+                  printf ("left side broken");
+                }
+
+              id_it = id_map.find (inode_id);
+              if (id_it == id_map.end())
+                {
+                  printf ("right side broken");
+                }
+              else
+                {
+                  // ID is reachable
+                  int& flags = id_it->second;
+                  flags |= 1;
+                }
+
+              n_links++;
+              if (output_needs_update())
+                links_update_status (n_links);
+              // printf ("%s -/%s/-> %s\n", id.pretty_str().c_str(), name.c_str(), inode_id.pretty_str().c_str());
+              //id_map[id] = 0;
+            }
+        }
+      dbc_ret = dbc->get (&key, &multi_data, DB_NEXT | DB_MULTIPLE_KEY);
+    }
+  links_update_status (n_links);
+  printf ("\n");
+}
+
 
 vector<string>
 IntegrityCheck::run()
 {
   read_all_ids();
+  check_links();
 
   vector<string> errs;
   errs.push_back ("foo");
