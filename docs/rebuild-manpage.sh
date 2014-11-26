1549fd0a6a7c1c679e805f72919551a65bce6c24
Stefan Westerfeld <stefan@space.twc.de>
2014-11-26 16:07:56 +0100

    DOCS: started to migrate documentation to asciidoc
    this also means: split command documentation to one page per command

--- docs/rebuild-manpage.sh
+++ docs/rebuild-manpage.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+for p in bfsync-clone.1 bfsync-commit.1
+do \
+  a2x -f manpage $p.txt
+  groff -mandoc -Thtml < $p >$p.html; \
+done
+exit
+
+# old documentation
 for p in bfsync.1 bfsyncfs.1
 do \
   curl -sfS http://testbit.eu/$p?action=render >$p.web; \
