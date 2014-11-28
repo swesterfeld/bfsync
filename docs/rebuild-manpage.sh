352e7b8d3435bdeeac58dc543b4d1e10c0d99af1
Stefan Westerfeld <stefan@space.twc.de>
2014-11-28 15:39:40 +0100

    DOCS: start work on bfsync-pull(1) manpage
    This also required adding our own version of the linkgit: macro.

--- docs/rebuild-manpage.sh
+++ docs/rebuild-manpage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for p in bfsync-clone.1 bfsync-commit.1 bfsync-push.1
+for p in bfsync-clone.1 bfsync-commit.1 bfsync-push.1 bfsync-pull.1
 do
   a2x -f manpage $p.txt
-  asciidoc -b xhtml11 -d manpage $p.txt
+  asciidoc -f asciidoc.conf -b xhtml11 -d manpage $p.txt
 done
 exit
 
