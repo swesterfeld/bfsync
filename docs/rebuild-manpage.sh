3ccf694bc6c668a2d48a63b0388b7bf3eaf24a41
Stefan Westerfeld <stefan@space.twc.de>
2014-12-01 12:27:25 +0100

    DOCS: added bfsync-put(1) manpage

--- docs/rebuild-manpage.sh
+++ docs/rebuild-manpage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for p in bfsync-clone.1 bfsync-commit.1 bfsync-push.1 bfsync-pull.1 \
-         bfsync-get.1
+         bfsync-get.1 bfsync-put.1
 do
   a2x -f manpage $p.txt
   asciidoc -f asciidoc.conf -b xhtml11 -d manpage $p.txt
