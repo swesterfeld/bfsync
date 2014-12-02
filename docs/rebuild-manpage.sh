73a68d3b88915beea3d0cf7978d3f260c3088980
Stefan Westerfeld <stefan@space.twc.de>
2014-12-02 14:51:20 +0100

    DOCS: added bfsync-delete-version(1) manpage

--- docs/rebuild-manpage.sh
+++ docs/rebuild-manpage.sh
@@ -5,7 +5,7 @@ for p in bfsync-clone.1 bfsync-commit.1 bfsync-push.1 bfsync-pull.1 \
          bfsync-revert.1 bfsync-continue.1 bfsync-need-continue.1 \
          bfsync-recover.1 bfsync-need-recover.1 bfsync-disk-usage.1 \
          bfsync-new-files.1 bfsync-expire.1 bfsync-upgrade.1 \
-         bfsync-need-upgrade.1
+         bfsync-need-upgrade.1 bfsync-delete-version.1
 do
   a2x -f manpage $p.txt
   asciidoc -f asciidoc.conf -b xhtml11 -d manpage $p.txt
