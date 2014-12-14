ea92f6c442165d3bf2e44180b3e9fd7f40023e7c
Stefan Westerfeld <stefan@space.twc.de>
2014-12-14 16:08:39 +0100

    DOCS: allow rebuilding only a selected set of pages

--- docs/rebuild-manpage.sh
+++ docs/rebuild-manpage.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
+PAGES="$@"
+if test -z "$PAGES"; then
+  PAGES=`awk < command-list.txt '{print $1".1"}'`
+  PAGES="$PAGES bfsync.1"
+fi
+
 cmd-list.py # generate command overview
 
-for p in bfsync-clone.1 bfsync-commit.1 bfsync-push.1 bfsync-pull.1 \
-         bfsync-get.1 bfsync-put.1 bfsync-check.1 bfsync-log.1 \
-         bfsync-gc.1 bfsync-repo-files.1 bfsync-status.1 bfsync-collect.1 \
-         bfsync-revert.1 bfsync-continue.1 bfsync-need-continue.1 \
-         bfsync-recover.1 bfsync-need-recover.1 bfsync-disk-usage.1 \
-         bfsync-new-files.1 bfsync-expire.1 bfsync-upgrade.1 \
-         bfsync-need-upgrade.1 bfsync-delete-version.1 \
-         bfsync-undelete-version.1 bfsync-config-set.1 \
-         bfsync-transfer-bench.1 bfsync-sql-export.1 bfsync-find-missing.1 \
-         bfsync-copy-expire.1 bfsync-get-repo-id.1 bfsync-diff.1 \
-         bfsync.1
+for p in $PAGES
 do
+  echo "processing $p"
   a2x -f manpage $p.txt
   asciidoc -f asciidoc.conf -b xhtml11 -d manpage $p.txt
 done
